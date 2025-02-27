15e8e0f9fdbb1f6006d48d76216dab50db7084e9
YoTstml <yohan.testemale@etu.inp-toulouse.fr>
2025-02-27 02:34:42 +0100

    chore: traefik values fix + half kanidm

---
+++ apps/traefik/apply.sh
@@ -0,0 +1,3 @@
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.3/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
+
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.3/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
